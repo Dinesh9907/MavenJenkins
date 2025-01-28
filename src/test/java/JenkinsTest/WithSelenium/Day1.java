1a1e1bfd479e5f4d1d42804889e2aad303e4444b
Dinesh9907 <ksdinesh2001@gmail.com>
2025-01-28 12:26:34 +0530

    Browser Param

--- src/test/java/JenkinsTest/WithSelenium/Day1.java
+++ src/test/java/JenkinsTest/WithSelenium/Day1.java
@@ -2,14 +2,17 @@ package JenkinsTest.WithSelenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 //Jenkins Integration using maven
 public class Day1 {
 	
+	@Parameters("Browser")
 	@Test
-	public void testit() throws InterruptedException {
+	public void testit(String Browser) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
+		System.out.println(Browser);
 		driver.manage().window().maximize();
 		driver.get("https://jpetstore.aspectran.com/");
 		System.out.println(driver.getTitle());
